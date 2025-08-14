b39c792c9d5dcdcd7821a2f7e602e414e0e9c194
aksel26 <kevinxkim2023@gmail.com>
2025-08-14 18:06:08 +0900

    svg 설정

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -7,6 +7,25 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;

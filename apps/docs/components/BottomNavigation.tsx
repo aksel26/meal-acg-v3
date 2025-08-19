32d72b2715aecb5c4b906c5406c32dca113ce6fa
aksel26 <kevinxkim2023@gmail.com>
2025-08-19 16:12:55 +0900

    하단 nav bar

--- apps/docs/components/BottomNavigation.tsx
+++ apps/docs/components/BottomNavigation.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import HomeIcon from "@/public/icons/home.png";
 import LunchIcon from "@/public/icons/lunch.png";
 import MonthlyIcon from "@/public/icons/monthly.png";
+import DNAIon from "@/public/icons/dna.png";
 
 export function BottomNavigation() {
   // 디버깅을 위한 로그
@@ -29,13 +30,18 @@ export function BottomNavigation() {
       label: "먼쓸리",
       icon: <Image src={MonthlyIcon} alt="monthly" height={30} />,
     },
+    {
+      id: "workDNA",
+      label: "유형검사",
+      icon: <Image src={DNAIon} alt="workDNA" height={30} />,
+    },
   ];
 
   const router = useRouter();
   const pathname = usePathname();
 
   return (
-    <nav className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-lg bg-white border-t border-gray-200 z-50 shadow-lg">
+    <nav className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-lg bg-white border-t border-gray-200 z-50 shadow-lg px-4 justify-between">
       <div className="flex justify-around items-center py-3">
         {navItems.map((item) => {
           const isActive = pathname === `/${item.id}`;
@@ -45,9 +51,13 @@ export function BottomNavigation() {
             <button
               key={item.id}
               onClick={() => {
-                router.push(`/${item.id}`);
+                if (item.id === "workDNA") {
+                  window.open("https://workdna.netlify.app/", "_blank");
+                } else {
+                  router.push(`/${item.id}`);
+                }
               }}
-              className={`flex flex-col items-center justify-center py-1 px-8 rounded-lg transition-all duration-200 ${
+              className={`flex flex-col items-center justify-center  py-1 w-1/4 rounded-lg transition-all duration-200 ${
                 isActive ? "text-blue-600 bg-blue-50" : "text-gray-500 hover:text-gray-700 hover:bg-gray-50"
               }`}
             >

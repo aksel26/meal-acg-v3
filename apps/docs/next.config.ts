f349dc279f2ca480e33bd8bac58866586b952b0d
aksel26 <kevinxkim2023@gmail.com>
2025-08-21 16:26:59 +0900

    pwa 세팅

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -4,19 +4,20 @@ import withPWA from "@ducanh2912/next-pwa";
 const nextConfig = {
   // PWA 설정을 활성화합니다.
   reactStrictMode: true,
-};
-
-const withPWAConfig = withPWA({
-  dest: "public", // 서비스 워커 파일이 생성될 디렉토리
-  disable: process.env.NODE_ENV === "development", // 개발 환경에서는 PWA 비활성화
-  // 더 많은 설정 옵션을 추가할 수 있습니다. 예를 들어, `register`나 `skipWaiting` 같은 옵션입니다.
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
+};
+
+const withPWAConfig = withPWA({
+  dest: "public", // 서비스 워커 파일이 생성될 디렉토리
+  // disable: process.env.NODE_ENV === "development", // 개발 환경에서는 PWA 비활성화
+  disable: false, // 개발 환경에서는 PWA 비활성화
+  // 더 많은 설정 옵션을 추가할 수 있습니다. 예를 들어, `register`나 `skipWaiting` 같은 옵션입니다.
+  webpack: (config: any) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,

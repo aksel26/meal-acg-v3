d6c435babf42f68c3371c4adc8acea12c27af971
aksel26 <kevinxkim2023@gmail.com>
2025-08-22 18:00:07 +0900

    remove component

--- apps/user/app/(content)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ContentHomePage() {
-  // Server-side redirect to dashboard as the default content page
-  redirect("/dashboard");
-}

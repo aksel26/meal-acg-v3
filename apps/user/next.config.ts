9f7363453465412c50318e7cef22a63da033bd60
aksel26 <kevinxkim2023@gmail.com>
2025-08-22 14:24:23 +0900

    build 테스트 : pwa 임시 해제

--- apps/user/next.config.ts
+++ apps/user/next.config.ts
@@ -1,8 +1,49 @@
-import type { NextConfig } from "next";
-import withPWA from "@ducanh2912/next-pwa";
+// import type { NextConfig } from "next";
+// import withPWA from "@ducanh2912/next-pwa";
 
+// const nextConfig = {
+//   // PWA 설정을 활성화합니다.
+//   reactStrictMode: true,
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// const withPWAConfig = withPWA({
+//   dest: "public", // 서비스 워커 파일이 생성될 디렉토리
+//   // disable: process.env.NODE_ENV === "development", // 개발 환경에서는 PWA 비활성화
+//   disable: false, // 개발 환경에서는 PWA 비활성화
+//   // 더 많은 설정 옵션을 추가할 수 있습니다. 예를 들어, `register`나 `skipWaiting` 같은 옵션입니다.
+//   webpack: (config: any) => {
+//     config.module.rules.push({
+//       test: /\.svg$/i,
+//       issuer: /\.[jt]sx?$/,
+//       use: ["@svgr/webpack"],
+//     });
+//     return config;
+//   },
+
+//   experimental: {
+//     turbo: {
+//       rules: {
+//         "*.svg": {
+//           loaders: ["@svgr/webpack"],
+//           as: "*.js",
+//         },
+//       },
+//     },
+//   },
+// });
+
+// export default withPWAConfig(nextConfig);
+
+// apps/user/next.config.js (임시 테스트용)
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // PWA 설정을 활성화합니다.
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
@@ -10,13 +51,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-
-const withPWAConfig = withPWA({
-  dest: "public", // 서비스 워커 파일이 생성될 디렉토리
-  // disable: process.env.NODE_ENV === "development", // 개발 환경에서는 PWA 비활성화
-  disable: false, // 개발 환경에서는 PWA 비활성화
-  // 더 많은 설정 옵션을 추가할 수 있습니다. 예를 들어, `register`나 `skipWaiting` 같은 옵션입니다.
   webpack: (config: any) => {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -36,6 +70,7 @@ const withPWAConfig = withPWA({
       },
     },
   },
-});
+};
 
-export default withPWAConfig(nextConfig);
+// PWA 래핑을 잠시 제거합니다.
+module.exports = nextConfig;

b52554ae7c7eddc0e2e3cb95fe8d4a1915b4077e
aksel26 <kevinxkim2023@gmail.com>
2025-12-24 14:39:24 +0900

    fix : snowflake count 조절

--- apps/user/app/(content)/layout.tsx
+++ apps/user/app/(content)/layout.tsx
@@ -31,7 +31,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
 
       <main className="container mx-auto px-4 py-6">
         <Snowfall
-          snowflakeCount={100}
+          snowflakeCount={50}
           speed={[0.5, 2]}
           radius={[5, 15]}
           images={images}

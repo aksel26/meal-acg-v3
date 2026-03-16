38cebb471de9737eeb0255998327c465b72c60fd
aksel26 <kevinxkim2023@gmail.com>
2026-03-16 19:01:52 +0900

    feat(part-time-supervisor): Sentry 에러 모니터링 통합
    - next.config.ts에 withSentryConfig 설정 추가
    - instrumentation.ts, instrumentation-client.ts 초기화 파일 추가
    - sentry.server.config.ts, sentry.edge.config.ts 설정 파일 추가
    - global-error.tsx Sentry 에러 캡처 컴포넌트 추가

---
+++ apps/part-time-supervisor/sentry.server.config.ts
@@ -0,0 +1,13 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "http://62e0e472dc33ebcaaec808ba0e1491da@115.68.68.138:9300/2",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});

d4419412a535e8fdc3796ca299d42ac5e58e5ea2
aksel26 <kevinxkim2023@gmail.com>
2026-04-03 02:44:14 +0900

    style: 콘텐츠 영역 max-width 제한 제거

--- apps/user/app/(content)/layout.tsx
+++ apps/user/app/(content)/layout.tsx
@@ -32,7 +32,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
           {/* Content */}
           <main className="relative flex-1 px-4 py-6 md:px-8 md:py-8">
-            <div className="mx-auto max-w-5xl">
+            <div className="mx-auto">
               <AnimatePresence mode="wait">
                 <motion.div
                   key={pathname}
